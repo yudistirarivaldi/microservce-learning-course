26e998af37aa2ece3978dcaddfa30c64bfa541bd
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2024-09-29 09:41:15 +0700

    first commit

---
+++ service-user/seeders/20200702155218-user-seeders.js
@@ -0,0 +1,32 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   
+    await queryInterface.bulkInsert('users', [
+      {
+        name: "widada",
+        profession: "Admin Micro",
+        role: "admin",
+        email: "juarakoding@gmail.com",
+        password: await bcrypt.hash('rahasia1234', 10),
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: "Yein",
+        profession: "Front End Developer",
+        role: "student",
+        email: "yein@mail.com",
+        password: await bcrypt.hash('rahasiaku123', 10),
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {   
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
